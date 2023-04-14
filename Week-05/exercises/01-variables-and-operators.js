b4ad3c4684f0ae4c2b97bf07d478fb0605a272d6
Marco Herazo <marcoherazo11@gmail.com>
2023-04-14 11:01:59 -0300

    BaSP-W05-2023: Create week-05 structure

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,7 @@
+//1. Variables y Operadores
+//1.a Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable
+
+var first = 20;
+var second = 30;
+var sum = first + second;
+console.log(sum);
