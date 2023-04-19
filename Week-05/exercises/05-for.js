8553c983c0a9a1edc69ec07a9dabffdc77c9e3bd
Marco Herazo <marcoherazo11@gmail.com>
2023-04-18 21:54:32 -0300

    BaSP-W05-2023: For

--- Week-05/exercises/05-for.js
+++ Week-05/exercises/05-for.js
@@ -2,12 +2,12 @@ console.log('*** EXERCISE 5: FOR');
 // Iterar sobre un array
 console.log('Exercise 5.A');
 var newArr = ["Hola", "que", "tal", "como", "estas"]
-for(let i = 0; i < newArr.length; i++){
+for(var i = 0; i < newArr.length; i++){
     alert(newArr[i]);
 }
 // Iterar sobre un array y convertir la primera letra en mayuscula de cada palabra
 console.log('Exercise 5.B');
-for(let i = 0; i < newArr.length; i++){
+for(var i = 0; i < newArr.length; i++){
     var word = newArr[i];
     var newWord = word.substring(0, 1).toUpperCase() + word.slice(1);
     alert(newWord);
@@ -15,14 +15,14 @@ for(let i = 0; i < newArr.length; i++){
 //
 console.log('Exercise 5.C');
 var sentence = "";
-for(let i = 0; i < newArr.length; i++){
+for(var i = 0; i < newArr.length; i++){
     sentence += newArr[i] + " ";
 }
 alert(sentence);
 //Crear un array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el número 0 hasta al número 9.
 console.log('Exercise 5.D');
 var arr = [];
-for (let i = 0; i < 10; i++){
+for (var i = 0; i < 10; i++){
     arr.push(i);
 }
 console.log(arr);
