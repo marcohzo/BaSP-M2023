3ba0f4b47bf1a7234ea68c6b8b77c957c52c6101
Marco Herazo <marcoherazo11@gmail.com>
2023-05-02 20:41:23 -0300

    BaSP-W07-2023: Fix variables names

--- Week-07/js/sign-up.js
+++ Week-07/js/sign-up.js
@@ -1,21 +1,24 @@
-const form = document.getElementById("myForm");
-const submitBtn = document.getElementById("submit-btn");
-const emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
-const nameInput = document.getElementById("name");
-const lastNameInput = document.getElementById("lastName");
-const dniInput = document.getElementById("dni");
-const birthInput = document.getElementById("dateOfBirth");
-const emailInput = document.getElementById("email");
-const localityInput = document.getElementById("locality");
-const zipCodeInput = document.getElementById("zipCode");
-const telephoneInput = document.getElementById("telephone");
-const addressInput = document.getElementById("address");
-const passwordInput = document.getElementById("password");
-const repeatPasswordInput = document.getElementById("repeatPassword");
+// Get references to DOM elements
+var form = document.getElementById("myForm");
+var submitBtn = document.getElementById("submit-btn");
+var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+var nameInput = document.getElementById("name");
+var lastNameInput = document.getElementById("lastName");
+var dniInput = document.getElementById("dni");
+var birthInput = document.getElementById("dateOfBirth");
+var emailInput = document.getElementById("email");
+var localityInput = document.getElementById("locality");
+var zipCodeInput = document.getElementById("zipCode");
+var telephoneInput = document.getElementById("telephone");
+var addressInput = document.getElementById("address");
+var passwordInput = document.getElementById("password");
+var repeatPasswordInput = document.getElementById("repeatPassword");
+
+// Set the input type for password fields to "password"
 passwordInput.setAttribute("type", "password");
 repeatPasswordInput.setAttribute("type", "password");
 
-// This function validates the input value of a given input field based on its type
+// Add event listeners to validate inputs when they lose focus
 function addInputValidation(type, errorMessage, input) {
   input.addEventListener("blur", function () {
     validateInput(input, type, errorMessage);
@@ -24,6 +27,8 @@ function addInputValidation(type, errorMessage, input) {
     clearError(input);
   });
 }
+
+// Add input validation for each input field
 addInputValidation("telephone", "Invalid telephone", telephoneInput);
 addInputValidation("name", "Invalid name", nameInput);
 addInputValidation("name", "Invalid last name", lastNameInput);
@@ -36,11 +41,12 @@ addInputValidation("locality", "Invalid locality", localityInput);
 addInputValidation("address", "Invalid address", addressInput);
 addInputValidation("date", "Invalid date", birthInput);
 
+// Validation functions
 function validateAlphanumeric(value) {
-  let letter = false;
-  let num = false;
+  var letter = false;
+  var num = false;
   for (let i = 0; i < value.length; i++) {
-    const charCode = value.charCodeAt(i);
+    var charCode = value.charCodeAt(i);
     if (
       (charCode >= 65 && charCode <= 90) ||
       (charCode >= 97 && charCode <= 122)
@@ -71,9 +77,9 @@ function validateSpace(value) {
 
 function showError(input, errorMessage) {
   input.classList.add("error");
-  const errorElement = input.parentNode.querySelector(".error-message");
+  var errorElement = input.parentNode.querySelector(".error-message");
   if (!errorElement) {
-    const newErrorElement = document.createElement("p");
+    var newErrorElement = document.createElement("p");
     newErrorElement.classList.add("error-message");
     newErrorElement.textContent = errorMessage;
     input.parentNode.insertBefore(newErrorElement, input.nextSibling);
@@ -82,26 +88,31 @@ function showError(input, errorMessage) {
   }
 }
 
+// Clear error message for an input field
 function clearError(input) {
-  const errorElement = input.parentNode.querySelector(".error-message");
+  var errorElement = input.parentNode.querySelector(".error-message");
   if (errorElement) {
     input.classList.remove("error");
     errorElement.remove();
   }
 }
+
 function validateDate(value) {
+  console.log(value);
   var currentDate = new Date();
-  var dateInput = value;
-  var date = dateInput.split("-");
-  var day = date[2];
-  var month = date[1];
-  var year = date[0];
-  var inputDate = new Date(year, month - 1, day);
-  return inputDate <= currentDate;
+  var date = new Date(value);
+  var month = date.getMonth() + 1;
+  var day = date.getDate() + 1;
+  var year = date.getFullYear();
+  var formattedDate = `${month.toString().padStart(2, "0")}/${day
+    .toString()
+    .padStart(2, "0")}/${year}`;
+  return { isValid: date <= currentDate, formattedDate };
 }
 
+// Validate input based on its type and show error message if necessary
 function validateInput(input, validationType, errorMessage) {
-  const value = input.value.trim();
+  var value = input.value.trim();
   if (validationType === "name") {
     if (value === "" || !isNaN(value) || value.length < 4) {
       showError(input, errorMessage);
@@ -123,7 +134,7 @@ function validateInput(input, validationType, errorMessage) {
     }
   }
   if (validationType === "telephone") {
-    if (value === "" || value.length !== 9 || isNaN(value)) {
+    if (value === "" || value.length !== 10 || isNaN(value)) {
       showError(input, errorMessage);
     }
   }
@@ -138,7 +149,8 @@ function validateInput(input, validationType, errorMessage) {
     }
   }
   if (validationType === "date") {
-    if (!validationDate(value)) {
+    var { isValid } = validateDate(value);
+    if (!isValid) {
       showError(input, errorMessage);
     }
   }
@@ -153,38 +165,53 @@ function validateInput(input, validationType, errorMessage) {
   }
 }
 
-function validateForm() {
-  const inputs = form.querySelectorAll("input");
-  const errors = [];
-  const result = [];
-  inputs.forEach(function (input) {
-    clearError(input);
-    if (input.value === "") {
-      errors.push(input.name);
-    } else {
-      validateInput(
-        input,
-        input.dataset.validationType,
-        input.dataset.errorMessage
-      );
-      if (!input.classList.contains("error")) {
-        result.push(input.name + ": " + input.value);
-      }
-    }
-  });
-  if (errors.length > 0) {
-    alert(`The following fields have errors: ${errors.join("\n")}`);
-  } else {
-    alert(
-      `The following fields have been successfully submitted:\n ${result.join(
-        "\n"
-      )}`
-    );
-    form.submit();
-  }
+var loginBaseUrl = "https://api-rest-server.vercel.app/signup";
+async function validateForm() {
+  // get the formatted date
+  var { formattedDate } = validateDate(birthInput.value.trim());
+  var response = await fetch(
+    `${loginBaseUrl}?email=${emailInput.value.trim()}&password=${passwordInput.value.trim()}&name=${nameInput.value.trim()}&lastName=${lastNameInput.value.trim()}&dni=${dniInput.value.trim()}&dob=${formattedDate}&phone=${telephoneInput.value.trim()}&address=${addressInput.value.trim()}&city=${localityInput.value.trim()}&zip=${zipCodeInput.value.trim()}`
+  );
+  var data = await response.json();
+
+  // display error message(s) in data.errors in an alert
+  !data.success
+    ? data.errors?.length > 0
+      ? alert(`Errores: ${data.errors.map((error) => error.msg).join(", ")}`)
+      : alert(data.msg)
+    : alert("Signup correcto");
+  // save the data obtained from the request in localStorage
+  localStorage.setItem("user", JSON.stringify(data.data));
 }
 
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
   validateForm();
 });
+
+function convertDateFormat(dateString) {
+  var dateArray = dateString.split("/");
+  var year = dateArray[2];
+  var month = dateArray[0].length === 1 ? `0${dateArray[0]}` : dateArray[0];
+  var day = dateArray[1].length === 1 ? `0${dateArray[1]}` : dateArray[1];
+  return `${year}-${month}-${day}`;
+}
+
+function loadFormDataFromLocalStorage() {
+  var formData = JSON.parse(localStorage.getItem("user"));
+  console.log(formData);
+  if (formData) {
+    nameInput.value = formData.name || "";
+    lastNameInput.value = formData.lastName || "";
+    dniInput.value = formData.dni || "";
+    birthInput.value = convertDateFormat(formData.dob) || "";
+    emailInput.value = formData.email || "";
+    localityInput.value = formData.city || "";
+    zipCodeInput.value = formData.zip || "";
+    telephoneInput.value = formData.phone || "";
+    addressInput.value = formData.address || "";
+    passwordInput.value = formData.password || "";
+    repeatPasswordInput.value = formData.password || "";
+  }
+}
+window.addEventListener("load", loadFormDataFromLocalStorage);
